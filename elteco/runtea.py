7abd285c58ca93d1325576f2490b378ac0ecfed5
DFuh <dafu@uni-bremen.de>
2022-06-16 09:33:28 +0200

    adapt runtea: use glob and filter

--- elteco/runtea.py
+++ elteco/runtea.py
@@ -3,7 +3,9 @@ main script
 define classes for economical assessment and simulation results
 run tea
 '''
-from aux.mainclasses import elEco#, elSimu
+import os
+import glob
+from auxf.mainclasses import elEco#, elSimu
 #import aux.handlefiles as hfs
 '''
 def mainrun():
@@ -28,9 +30,41 @@ def mainrun():
 if __name__ == '__main__':
     #mainrun()
     print('...run...')
-    tea_inst = elEco()
-    tea_inst.run_tea()
+    # inp = input('Please insert dirname: ')
+    # inp = '/home/dafu_res/01_data/epos_calc_out/main_runs_202203/20220312'
+    if False:#True:
+        pass
+        #inp = '/home/dafu_res/01_data/epos_calc_out/main_runs_202205/20220430'
+        #if inp !='':
+        #    sdir = inp
+        #    dirs = os.walk(sdir)
+            # bpth = dirs[0]
+        #n=0
+        #for root,dir,fls in dirs:
+        #    if root == inp:
+        #        for dirnm in dirs:
+        #            print('dirnm: ', dirnm[0])
+        #            inpi = input(f'Apply for {dirnm[0]} (y/ any key)')
+        #            if 'y' in inpi.lower():
+        #                tea_inst = elEco(pth_data_loc=os.path.join(root,dirnm[0]))
+        #                #tea_inst.run_tea()
+    if True:
+        inp = input('Please insert dirname: ')
+        lst = glob.glob(os.path.join(inp,'*matbal.csv'))
 
+        fltr = input('Enter Filter for list: (skip: Enter)')
+        if fltr !='':
+            lst = [item for item in lst if fltr in item]
+
+        for j,sf in enumerate(lst):
+            print(f'[{j}] ->| ', sf)
+            slct = input('Run Simulation? (y:Enter/ n:any key): ')
+            if slct=='':
+                tea_inst = elEco(pth_data_loc=os.path.dirname(sf))
+                tea_inst.run_tea()
+    else:
+        tea_inst = elEco()
+        tea_inst.run_tea()
     #for num,inst in enumerate(tea_inst.simuinst):
 
         #print('num: ', num)
